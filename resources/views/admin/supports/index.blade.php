9a991da3acb46b7f238c8befc8f77a7ffbb268b8
Lucas Miotto <latmiotto@gmail.com>
2023-10-12 18:57:14 -0300

    feat: criando e utilizando template.

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,26 +1,37 @@
-<h1>Listagem dos Suportes</h1>
-<a href="{{ route('supports.create') }}">Criar Dúvida</a>
+@extends('admin.layouts.app')
 
-<table>
-    <thead>
-        <th>Assunto</th>
-        <th>Status</th>
-        <th>Descrição</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}"> ir -> </a>
-                    <a href="{{ route('supports.edit', $support->id) }}"> Editar -> </a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('title', 'Fórum')
 
-<x-pagination :paginator="$supports" :appends="$filters" />
+@section('header')
+    <h1>Listagem dos Suportes</h1>
+@endsection
+
+
+@section('content')
+    <a href="{{ route('supports.create') }}">Criar Dúvida</a>
+
+    <table>
+        <thead>
+            <th>Assunto</th>
+            <th>Status</th>
+            <th>Descrição</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach ($supports->items() as $support)
+                <tr>
+                    <td>{{ $support->subject }}</td>
+                    <td>{{ getStatusSupport($support->status) }}</td>
+                    <td>{{ $support->body }}</td>
+                    <td>
+                        <a href="{{ route('supports.show', $support->id) }}"> ir -> </a>
+                        <a href="{{ route('supports.edit', $support->id) }}"> Editar -> </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <x-pagination :paginator="$supports" :appends="$filters" />
+
+@endsection
