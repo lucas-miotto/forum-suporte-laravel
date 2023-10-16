8f767237cab7fa4c5c141d021925c27914a89130
Lucas Miotto <latmiotto@gmail.com>
2023-10-16 11:21:01 -0300

    feat: finalizando pagina show.

--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -1,13 +1,21 @@
-<h1>Detalhes da duvida {{ $support->id }}</h1>
+@extends('admin.layouts.app');
 
-<ul>
-    <li>Assunto {{ $support->subject }}</li>
-    <li>Status {{ $support->status }}</li>
-    <li>Descrição {{ $support->body }}</li>
-</ul>
+@section('title', "Detalhes da Dúvida {$support->subject}")
 
-<form action="{{ route('supports.destroy', $support->id) }}" method="post">
-    @csrf
-    @method('DELETE')
-    <button type="submit">DELETAR</button>
-</form>
+@section('header')
+    <h1 class="text-lg text-black-500">Dúvida {{ $support->subject }}</h1>
+@endsection
+
+@section('content')
+    <ul class="list-disc">
+        <li>Assunto {{ $support->subject }}</li>
+        <li>Status {{ $support->status }}</li>
+        <li>Descrição {{ $support->body }}</li>
+    </ul>
+    <form action="{{ route('supports.destroy', $support->id) }}" method="post">
+        @csrf
+        @method('DELETE')
+        <button type="submit"
+            class="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded my-4">Deletar</button>
+    </form>
+@endsection
