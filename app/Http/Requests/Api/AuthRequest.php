e22977735923b6cdde2ff456b2e3114e5584d262
Lucas Miotto <latmiotto@gmail.com>
2023-11-22 10:49:28 -0300

    feat: implementando Autenticação de APIs no Laravel com Sanctum. Login, logout e recuperando o usuario.

---
+++ app/Http/Requests/Api/AuthRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+            ],
+            'password' => [
+                'required',
+                'max:255',
+            ],
+            'device_name' => [
+                'required',
+                'max:255',
+            ]
+        ];
+    }
+}
